2924e45f9e135f2481e964c805dd7c0eab515451
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-09-05 10:33:40 -0700

    task 15

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -2,8 +2,7 @@ def magic_calculation(a, b, c):
     if a < b:
         result = a + b
         if c < b:
-            result -= 1
-    else:
-        result = c + b
-
-    return result
+            result *= 10
+        else:
+            result = c + b
+        return result
