210657ce486ce71363242dad079032022fb2a275
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-09-05 05:26:00 -0700

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 my_range = range(97, 123)
+output_string = ""
 for ascii_value in my_range:
-    print(f"{chr(ascii_value)}", end = "")
+    output_string += f"{chr(ascii_value):s}"
+
+print(output_string, end = "")
