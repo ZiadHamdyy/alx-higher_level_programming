7235115ba1e63781b10283d4ee332b4666a9020e
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-09-07 06:12:42 -0700

    task 4 finally

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
-import pyclbr
-
-def print_names_from_pyc(pyc_file_path):
-    try:
-        module_data = pyclbr.readmodule_ex('hidden_4', [pyc_file_path])
-        names = [name for name, item in module_data.items() if not name.startswith('__')]
-
-        sorted_names = sorted(names)
-        for name in sorted_names:
-            print(name)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
 if __name__ == "__main__":
-    pyc_file_path = "hidden_4.pyc"
-    print("Printing names from compiled module:")
-    print_names_from_pyc(pyc_file_path)
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[0:2] != "__":
+            print(i)
