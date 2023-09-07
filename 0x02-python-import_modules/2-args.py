2f418e2add42ff2dd412189667a45152cdde4d36
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-09-07 04:05:01 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    import sys
+
+    arguments = sys.argv
+    number = len(arguments)
+    if number == 1:
+        print("{} argument:".format(number))
+    else:
+        print("{} arguments:".format(number - 1))
+
+    for i in range(1, number):
+        print("{}: {}".format(i, arguments[i]))
