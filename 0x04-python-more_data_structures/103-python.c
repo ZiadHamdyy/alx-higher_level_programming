406946312628dc20e1958429d146f7a052d15e99
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-09-13 19:12:53 +0000

    task 16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,10 +1,13 @@
-#define PY_SSIZE_T_CLEAN
+SIZE_T_CLEAN
 #include <Python.h>
 #include <stdio.h>
 
-void print_python_bytes(PyObject *p) {
+void print_python_bytes(PyObject *p)
+{
     printf("[.] bytes object info\n");
-    if (!PyBytes_Check(p)) {
+
+    if (!PyBytes_Check(p))
+    {
         printf("  [ERROR] Invalid Bytes Object\n");
         return;
     }
@@ -13,20 +16,25 @@ void print_python_bytes(PyObject *p) {
     printf("  size: %zd\n", size);
 
     char *str = PyBytes_AsString(p);
-    if (str == NULL) {
+    if (str == NULL)
+    {
         printf("  trying string: <nil>\n");
-    } else {
+    }
+    else
+    {
         printf("  trying string: %s\n", str);
     }
 
     printf("  first 10 bytes: ");
-    for (Py_ssize_t i = 0; i < 10 && i < size; i++) {
+    for (Py_ssize_t i = 0; i < 10 && i < size; i++)
+    {
         printf("%02x ", (unsigned char)str[i]);
     }
     printf("\n");
 }
 
-void print_python_list(PyObject *p) {
+void print_python_list(PyObject *p)
+{
     PyListObject *list = (PyListObject *)p;
 
     printf("[*] Python list info\n");
@@ -36,22 +44,36 @@ void print_python_list(PyObject *p) {
     Py_ssize_t allocated = list->allocated;
     printf("[*] Allocated = %zd\n", allocated);
 
-    for (Py_ssize_t i = 0; i < size; i++) {
+    for (Py_ssize_t i = 0; i < size; i++)
+    {
         PyObject *item = PyList_GetItem(p, i);
         printf("Element %zd: ", i);
-        if (PyBytes_Check(item)) {
+        if (PyBytes_Check(item))
+        {
             print_python_bytes(item);
-        } else if (PyLong_Check(item)) {
+        }
+        else if (PyLong_Check(item))
+        {
             printf("int\n");
-        } else if (PyFloat_Check(item)) {
+        }
+        else if (PyFloat_Check(item))
+        {
             printf("float\n");
-        } else if (PyTuple_Check(item)) {
+        }
+        else if (PyTuple_Check(item))
+        {
             printf("tuple\n");
-        } else if (PyList_Check(item)) {
+        }
+        else if (PyList_Check(item))
+        {
             printf("list\n");
-        } else if (PyUnicode_Check(item)) {
+        }
+        else if (PyUnicode_Check(item))
+        {
             printf("str\n");
-        } else {
+        }
+        else
+        {
             printf("<other>\n");
         }
     }
