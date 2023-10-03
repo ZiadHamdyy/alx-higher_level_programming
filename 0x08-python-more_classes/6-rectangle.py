5087af421e0ed2b98a2c7b517a2ba0a5269b04e8
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-10-02 18:16:31 -0700

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Init"""
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
