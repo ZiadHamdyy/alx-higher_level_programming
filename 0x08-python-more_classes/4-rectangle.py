d53621ac7c8b6f2027281667791a1e13bcf5ab3b
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-10-02 18:01:01 -0700

    task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""class"""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """rectangle."""
 
     def __init__(self, width=0, height=0):
         """Init"""
@@ -45,3 +45,22 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """printable representation"""
+
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
+
+    def __repr__(self):
+        """string representation"""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
