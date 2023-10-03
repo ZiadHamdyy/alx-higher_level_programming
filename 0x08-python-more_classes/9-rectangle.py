ff1093c3b432583382115f72db8896102a1979ba
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-10-02 18:44:33 -0700

    task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -85,3 +85,8 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return (rect_1)
         return (rect_2)
+
+    @classmethod
+    def square(cls, size=0):
+        """square"""
+        return (cls(size, size))
