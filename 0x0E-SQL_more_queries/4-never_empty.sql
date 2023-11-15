85544f781f60a308c9840a28d043c4266a073479
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-11-15 16:28:49 +0200

    task 4, 5 requirments

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,10 +1,10 @@
 -- creates the table id_not_null on your MySQL server.
-SELECT 1 FROM information_schema.tables 
+SELECT 1 FROM tables 
   WHERE table_schema = DATABASE() 
   AND table_name = 'id_not_null' 
   LIMIT 1;
 
 CREATE TABLE IF NOT EXISTS (
-  id INT DEFAULT 1,
+  id INT NOT NULL DEFAULT 1,
   name VARCHAR(256)
 );
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,10 +1,11 @@
 -- creates the table unique_id on your MySQL server.
-SELECT 1 FROM information_schema.tables 
+SELECT 1 FROM tables 
   WHERE table_schema = DATABASE() 
   AND table_name = 'unique_id' 
   LIMIT 1;
 
 CREATE TABLE IF NOT EXISTS (
-  id INT DEFAULT 1 UNIQUE,
+  id INT DEFAULT 1,
   name VARCHAR(256)
+  UNIQUE (id)
 );
