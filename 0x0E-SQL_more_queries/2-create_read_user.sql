c563c8bb44f5aa97bc3b76984aeeef1d9b45d5e3
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-11-15 16:17:33 +0200

    task 3

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,6 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grant SELECT privilege on the database to user_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
-
--- Flush privileges to apply changes
+USE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
