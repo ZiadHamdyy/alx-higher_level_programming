87c51bf2727030674a477f665c86179dd8f31344
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-11-15 16:44:18 +0200

    task 6

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,10 +1,5 @@
 -- creates the table id_not_null on your MySQL server.
-SELECT 1 FROM tables 
-  WHERE table_schema = 'hbtn_0d_2' 
-  AND table_name = 'id_not_null' 
-  LIMIT 1;
-
 CREATE TABLE IF NOT EXISTS id_not_null (
-  id INT NOT NULL DEFAULT 1,
+  id INT DEFAULT 1,
   name VARCHAR(256)
 );
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,11 +1,5 @@
 -- creates the table unique_id on your MySQL server.
-SELECT 1 FROM tables 
-  WHERE table_schema = DATABASE() 
-  AND table_name = 'unique_id' 
-  LIMIT 1;
-
 CREATE TABLE IF NOT EXISTS (
-  id INT DEFAULT 1,
+  id INT DEFAULT 1 UNIQUE,
   name VARCHAR(256)
-  UNIQUE (id)
 );
