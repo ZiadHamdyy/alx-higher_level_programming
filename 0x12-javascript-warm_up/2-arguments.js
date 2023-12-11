01350edfe13716d1e8ec6ef7ced126cefd547256
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-12-11 17:24:51 +0200

    task 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 let i = 0;
-while (process.argv[i])
- i++;
-if (i == 2)
-  console.log('No argument');
-else if (i == 3)
-  console.log('Argument found');
-else
-  console.log('Arguments found');
+while (process.argv[i]) {
+  i++;
+}
+if (i === 2) { console.log('No argument'); } else if (i === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
