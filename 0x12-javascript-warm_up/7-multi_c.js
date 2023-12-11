ddabc8fc52de86da91384f3dee76333fa150c3bc
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-12-11 19:25:04 +0200

    task 8

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 let i = 0;
-if (parseInt(process.argv[2]))
-  while (i < parseInt(process.argv[2]))
-  {
+if (parseInt(process.argv[2])) {
+  while (i < parseInt(process.argv[2])) {
     console.log('C is fun');
     i++;
   }
-else
-  console.log('Missing number of occurrences');
+} else { console.log('Missing number of occurrences'); }
