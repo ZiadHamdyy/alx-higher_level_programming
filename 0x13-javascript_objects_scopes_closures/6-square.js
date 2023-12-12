341b619419e40e5e6116f5b3ab03461a878774d5
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-12-12 18:36:45 +0200

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const square = require('./5-square');
+class Square extends square {
+  charPrint (c) {
+    const char = c || 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
