ff98ad03ed7d34e14216e33115919ad577ed1ddd
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-12-16 20:54:36 +0200

    task 15

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -2,8 +2,9 @@
 """class definition of a City"""
 
 
+from enum import unique
 from sqlalchemy import Column, ForeignKey, Integer, String
-from model_state import Base
+from relationship_state import Base, State
 
 
 class City(Base):
@@ -11,8 +12,10 @@ class City(Base):
     cities table:
         id: primary key
         name: name of the city
+        state_id: id of State
     """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True,
+                unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
