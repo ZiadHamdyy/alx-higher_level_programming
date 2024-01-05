6ecd14d40c982e0c03215b728168238492f84cc6
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-01-05 23:52:43 +0200

    task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL and displays all HTTP methods the server will accept.
-curl -s -i -X OPTIONS "$url" | grep -i 'allow' | cut -d':' -f2 | tr -d '[:space:]'
+curl -si -X OPTIONS "$1" | grep -i 'Allow' | cut -d':' -f2 | tr -d '[:space:]'
