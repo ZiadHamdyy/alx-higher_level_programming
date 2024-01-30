8cefc203c829f7e7e80d09702e69e1f73e67d7ac
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-01-30 20:35:02 +0200

    task 8 advanced

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,22 +2,20 @@
 
 const request = require('request');
 
-request('https://swapi.dev/api/films/' + process.argv[2], (error, response, body) => {
-  if (error) {
-    return console.log(error);
+request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (err, res, body) => {
+  if (err) {
+    return console.log(err);
   }
-
-  const film = JSON.parse(body);
-  const characters = film.characters;
-
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        return console.log(charError);
+  const data = JSON.parse(body);
+  data.characters.map(character => {
+    request.get(character, (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      } else {
+        const name = JSON.parse(body).name;
+        console.log(name);
       }
-
-      const character = JSON.parse(charBody);
-      console.log(character.name);
     });
+    return 0;
   });
 });
