5c639789be378f1026b4fdff4f67f45646142cf2
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-01-30 18:03:54 +0200

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('Status Code:', res.statusCode);
+});
